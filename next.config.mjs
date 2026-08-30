55c6ddb47e5edc24f6e0d3be0060f1335a029514
Mantavya Rawat <rawat.mantavya@gmail.com>
2026-08-30 15:59:42 +0530

    Final

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-  distDir: process.env.NODE_ENV === "production" ? ".next-production" : ".next",
 };
 
 export default nextConfig;
